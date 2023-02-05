d593623c08d48e6823019584385e4e9677c261ed
Zero Grad <112247214+z0grad@users.noreply.github.com>
2023-02-05 23:48:24 +0200

    Add files via upload

---
+++ Main Page.py
@@ -0,0 +1,27 @@
+# Main Streamlit page
+
+import streamlit as st
+
+st.title('Covid-19 Data Analysis')
+st.markdown('''
+This app is created to analyze the Covid-19 data.
+''')
+
+# Image
+st.image('https://www.who.int/images/default-source/health-topics/coronavirus/covid-19-who-5-1-2020.jpg?sfvrsn=3b1b1b1c_2', width=300)
+
+# Description
+st.header('Description')
+st.markdown('''
+This app is created to analyze the Covid-19 data.
+''')
+
+
+
+
+
+
+
+
+
+
